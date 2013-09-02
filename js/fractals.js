634b3ab3f6f34b129780e3231172f17064eb90bc
Dmitry Alimov <dvalimov@gmail.com>
2013-09-03 01:56:10 +0700

    Visualization of the Julia set is added

--- js/fractals.js
+++ js/fractals.js
@@ -21,6 +21,8 @@ function render() {
 	context.clearRect(0, 0, canvas.width , canvas.height);
 	// visualize the Mandelbrot set
 	drawMandelbrot();
+	// visualize the Julia set
+	//drawJulia();
 }
 
 function drawMandelbrot() {
@@ -58,4 +60,42 @@ function drawMandelbrot() {
 
 	// draw image
 	context.putImageData(image_data, 0, 0);	
+}
+
+
+function drawJulia() {
+	// prepare image and pixels
+	var image_data = context.createImageData(canvas.width, canvas.height);
+	var d = image_data.data;
+
+	x0 = -0.4;
+	y0 = -0.6;
+	max_iterations = 100;
+	for (var i = 0; i < canvas.height; i++) {
+		for (var j = 0; j < canvas.width; j++) {
+	
+			// limit the axis
+			x = -1.5 + j * 3.0 / canvas.width;
+			y = -1.0 + i * 2.0 / canvas.height;
+			
+			iteration = 0;
+			
+			while ((x * x + y * y < 4) && (iteration < max_iterations)) {
+				x_n = x * x - y * y + x0;
+				y_n = 2 * x * y + y0;
+				x = x_n;
+				y = y_n;
+				iteration++;
+			}
+			
+			// set pixel color [r,g,b,a]
+			d[i * canvas.width * 4 + j * 4 + 0] = iteration*25;
+			d[i * canvas.width * 4 + j * 4 + 1] = iteration*5;
+			d[i * canvas.width * 4 + j * 4 + 2] = iteration*8;
+			d[i * canvas.width * 4 + j * 4 + 3] = 255;
+		}		
+	}
+
+	// draw image
+	context.putImageData(image_data, 0, 0);
 }
